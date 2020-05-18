584d69d29463508d69816a8c9466ab0766fdd3c1
henrany <tmkhenry@gmail.com>
2020-05-18 15:00:37 -0300

    adding generic implementation of the bubble sort algorithm

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -4,23 +4,24 @@ using namespace std;
 
 template<typename T>
 
-void bubbleSort(vector<T> bubble){
+vector<T> bubbleSort(vector<T> bubble){
+    T temp;
     for(int i=0;i<bubble.size();i++){
         for(int j=0;j<bubble.size()-1;j++){
             if(bubble[j]>bubble[j+1]){
-                T temp = bubble[j];
+                temp = bubble[j];
                 bubble[j] = bubble[j+1];
                 bubble[j+1] = temp;
             }
         }
     }
+    return bubble;
 }
 
 int main(){
     vector<int> bubble {1,3,8,2,9,2,5,6};
-    bubbleSort(bubble);
     cout<<"[ ";
-    for(auto &i: bubble){
+    for(auto &i:bubbleSort(bubble)){
         cout<<i<<" ";
     }
     cout<<" ]\n";
