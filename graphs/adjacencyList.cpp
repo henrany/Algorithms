90a7bb92b2370167603f79ab5725d242261db53b
henrany <tmkhenry@gmail.com>
2020-08-11 16:50:00 -0300

    graph representation using adjacency list with stls

---
+++ graphs/adjacencyList.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+//implementation of the adjacency list of graph using stls
+
+std::list<int> adjacencyList[];
+
+//adding vertices and edges to the list
+void add_edge(int numOfVertices){
+    for(int i=0;i<numOfVertices;i++){
+        int u=0,v=0;
+        std::cin>>u>>v;
+        adjacencyList[u].push_back(v);
+        adjacencyList[v].push_back(u);
+    }
+}
+
+//display the list
+void displayGraph(int numOfVertices){
+    for(int i=0;i<numOfVertices;i++){
+        std::cout<<"vertex "<<i;
+        for(auto a : adjacencyList[i]){
+            std::cout<<"-->"<<a;
+        }
+        std::cout<<"\n";
+    }
+}
+
+int main(){
+    int numOfVertices =0;
+    std::cin>>numOfVertices;
+    add_edge(numOfVertices);
+    displayGraph(numOfVertices);
+}
